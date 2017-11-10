463e0a346e82111e6304ce5bfcae4fd8e1bf708a
Daniel Gallagher <daniel.gallagher@bbc.co.uk>
2017-11-10 13:44:03 +0000

    Removed undefined from the type mix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export function relyOn(expectedNode: Node | null | undefined, errorMessage: string = 'The element is required.'): Node {
-    if (expectedNode === null || expectedNode === undefined) {
+export function relyOn(expectedNode: Node | null, errorMessage: string = 'The element is required.'): Node {
+    if (expectedNode === null) {
         throw new TypeError(errorMessage);
     } else if (!(expectedNode instanceof Node)) {
         throw new TypeError('The argument must be an HTMLElement or a Node.')
