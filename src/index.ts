8e87442093f57663a0bb2d0ab0a4b025ff1d3add
Daniel Gallagher <daniel.gallagher@bbc.co.uk>
2017-11-22 23:08:41 +0000

    refactor(style): Format code via Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-export function relyOn(expectedNode: Node | null, errorMessage: string = 'The element is required.'): Node {
-    if (expectedNode === null) {
-        throw new TypeError(errorMessage);
-    } else if (!(expectedNode instanceof Node)) {
-        throw new TypeError('The argument must be an HTMLElement or a Node.')
-    } else {
-        return expectedNode;
-    }
+export function relyOn(
+  expectedNode: Node | null,
+  errorMessage: string = "The element is required."
+): Node {
+  if (expectedNode === null) {
+    throw new TypeError(errorMessage);
+  } else if (!(expectedNode instanceof Node)) {
+    throw new TypeError("The argument must be an HTMLElement or a Node.");
+  } else {
+    return expectedNode;
+  }
 }
